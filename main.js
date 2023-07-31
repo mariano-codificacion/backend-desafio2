137f2e6e8c4605925d9a00d945e433fabec61563
mtoranzo <marianoeto@hotmail.com>
2023-07-31 19:19:14 -0300

    elimino thisproducts

--- main.js
+++ main.js
@@ -5,33 +5,35 @@ import { promises as fs } from 'fs'
 
 class ProductManager {
     constructor(path) {
-        this.products = [];
+       
         this.path = path;
     }
 
 
     //Retornar todos los productos
-    getProducts() {
-        //this.products = JSON.parse(fs.readFile(this.path, 'utf-8'));
+    async getProducts() {
+        let products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
         //this.products = JSON.parse(this.path, 'utf-8'))
-        console.log(this.products);
+        console.log(products);
     }
+
     async addProduct(product) {
-        //this.products = JSON.parse(fs.readFile(this.path, 'utf-8'));
+        let products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
         //Consulto si mi producto ya existe en el txt
-        if (this.products.find(producto => producto.id == product.id)) {
+        if (products.find(producto => producto.id == product.id)) {
             return "Producto ya agregado"
         }
         //Lo agrego al array al ya saber que no existe
-        this.products.push(product)
+        products.push(product)
         //Parsearlo y guardar el array modificado
-        await fs.writeFile(this.path, JSON.stringify(this.products))
-        return this.products
+        await fs.writeFile(this.path, JSON.stringify(products))
+        return products
     }
-    getProductById(id) {
+
+    async getProductById(id) {
         //En el productManager, la ruta esta en this.path
-        //this.products = JSON.parse(fs.readFile(this.path, 'utf-8'));
-        const buscado = this.products.find(producto => producto.id === id)
+        let products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
+        const buscado = products.find(producto => producto.id === id)
         if (buscado) {
             console.log(buscado)
         } else {
@@ -41,41 +43,41 @@ class ProductManager {
     
     async updateProduct (id, { title, description, price, thumbnail, code, stock, }) {
 
-        this.products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
+        let products = JSON.parse(await fs.readFile(this.path, 'utf-8'));
 
-        const indice = this.products.findIndex(prod => prod.id === id)
+        const indice = products.findIndex(prod => prod.id === id)
     
         if (indice != -1) {
             //Mediante el indice modifico todos los atributos de mi objeto
-            this.products[indice].title = title
-            this.products[indice].description = description
-            this.products[indice].price = price
-            this.products[indice].thumbnail = thumbnail
-            this.products[indice].code = code
-            this.products[indice].stock = stock
+            products[indice].title = title
+            products[indice].description = description
+            products[indice].price = price
+            products[indice].thumbnail = thumbnail
+            products[indice].code = code
+            products[indice].stock = stock
             
-            await fs.writeFile(this.path, JSON.stringify(this.products))
-            console.log(this.products);
+            await fs.writeFile(this.path, JSON.stringify(products))
+        
         } else {
             console.log("Producto no encontrado")
         }
-       
     }
     
     async deleteProduct (id) {
-        //this.products = JSON.parse(fs.readFile(this.path, 'utf-8'));
-        const buscado = this.products.find(item => item.id === id);
+        
+        let products =  JSON.parse(await fs.readFile(this.path, 'utf-8'));
+        const buscado = products.find(item => item.id === id);
         if (!buscado) {
             console.log("error: not found");
         }
-        const prods = this.products.filter(prod => prod.id != id)
-        this.products = prods;
-        await fs.writeFile(this.path, JSON.stringify(this.products))
+        const prods = products.filter(prod => prod.id != id)
+        products = prods;
+        await fs.writeFile(this.path, JSON.stringify(products))
     }
     
 }
 
-const productManager = new ProductManager('prueba.txt')
+const productManager = new ProductManager('./prueba.txt')
 const product1 = new Product("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
 //const product2 = new Product("producto prueba", "Este es un producto prueba", 220, "Sin imagen", "abc124", 20)
 productManager.getProducts();
@@ -84,10 +86,10 @@ productManager.addProduct(product1)
 //productManager.getProducts();
 console.log(productManager.getProductById(2))
 productManager.deleteProduct(3)
-console.log(productManager.getProducts())
+productManager.getProducts();
 productManager.updateProduct(1,{title: "zanahoria",description: "Este es un producto prueba", price: 200, thumbnail: "Sin imagen", code: "abc123", stock: 20})
 //productManager.deleteProduct(2)
 //console.log(productManager.getProductById(1))
-//console.log(productManager.getProducts())
+productManager.getProducts();
 //productManager.deleteProduct(2)
 //console.log(productManager.getProducts())
